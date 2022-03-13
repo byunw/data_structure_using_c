27bcb7ded528af2e72012839a6da273c2cef853b
byunw <wbyun1461@gmail.com>
2022-03-13 11:09:43 +0900

    Create calc_sentence_length.c

---
+++ calc_sentence_length.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    
+    char buffer[30];
+    printf("길이 20이하의 문장만 입력해주세요: "); 
+    gets(buffer);
+    printf("%lu\n",strlen(buffer));
+    return 0;
+            
+}
