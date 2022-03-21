1437426f0e1eb13d76fd5ec04947f8790a898af5
byunw <wbyun1461@gmail.com>
2022-03-21 22:10:17 +0900

    Create output_charinputlength.c

---
+++ output_charinputlength.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+
+
+//이 프로그램은 alphabet input 길이 3이하에서만 작동함
+
+int read_line(char str[],int n){
+    
+    //각각의 char input 담을 공간
+    char ch;
+    
+    //인풋길이
+    int i=0;
+    
+    //ch에 각각의 char 저장하고 저장한 이진수 값이 '\n'랑 다른걸 의미할경우 계속 룹
+    while((ch=getchar())!='\n')
+      
+      //할당된 어레이의 허용된 공간만 접근하게
+      if(i<n)
+         //인풋을 의미하는 이진수 값을 ch에서 어레이로 복사 
+         str[i++]=ch;
+         
+      //인풋길이 반환    
+      return i;
+      
+
+}
+
+int main(void){
+    
+    //char input 담을 어레이 마지막 byte에는 '\n' 의미하는 00000000 저장 
+    char buffer[4];
+    
+    //인풋 길이 저장할 변수
+    int k;
+    
+    //어레이 사이즈 저장할 변수 
+    int n=4;
+   
+    while(1){
+      
+      printf("$ ");
+      //인풋길이 반환받고 k에 저장
+      k=read_line(buffer,n);
+      
+      //어레이에 저장된 char input이랑 길이 찍어주기
+      printf("%s:%d\n",buffer,k);
+      
+    }
+
+
+
+}
