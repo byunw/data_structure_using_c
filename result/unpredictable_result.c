d97cf3b9d7a285f0e3362efd7b2f4a06f01f7533
byunw <wbyun1461@gmail.com>
2022-04-03 18:23:09 +0900

    Update unpredictable_result.c

--- result/unpredictable_result.c
+++ result/unpredictable_result.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
 int main(void){
-        
+            
     char buffer[4];
     buffer[0]='a';
     printf("%s",buffer);
     return 0;
-    
+
 }
 
-//예상 output: a
-//실제 output: a0?
+//내 예샹 프로그램 출력 값: a
+//실제 프로그램 출력 값이 매 executable 실행때마다 다름
+    //위의 프로그램 실행시 출력되는 결과 리스트 
+        //a
+        //a
+        //a@i
+        
 
